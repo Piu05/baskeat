2dc7ef52c8560fbef0b9219ffd8a87b32cbc1a95
Piu05 <priyanka050412@gmail.com>
2022-09-10 14:21:26 +0530

    Pages created

---
+++ src/pages/college/Addcollege.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Button } from "@mui/material";
+import { Box } from "@mui/system";
+import TextField from "@mui/material/TextField";
+import { Link } from "react-router-dom";
+
+import "./Addcollege.css";
