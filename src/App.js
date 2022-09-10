071cdeb5a40f411c13ed6a729ac9c96ebbc2a3bd
Anubha13kumari <anubhamuz@gmail.com>
2022-09-10 14:56:49 +0530

    stationary

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import "./App.css";
 import StatHome from "./pages/stationary/StatHome";
-// import HeroPage from "./pages/login-pages/heroPage";
-// import CantHome from "./pages/canteen/Home";
-// import CantMenu from "./pages/canteen/Menu";
-// import CantCart from "./pages/canteen/Cart";
+import HeroPage from "./pages/login-pages/heroPage";
+import CantHome from "./pages/canteen/Home";
+import CantMenu from "./pages/canteen/Menu";
+import CantCart from "./pages/canteen/Cart";
 import StatCart from "./pages/stationary/StatCart";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-// import AdminLogin from "./pages/login-pages/AdminLogin";
-// import StudentLogin from "./pages/login-pages/StudentLogin";
-// import Clgsearch from "./pages/college/Clgsearch";
-// import Addcollege from "./pages/college/Addcollege";
-// import SignUp from "./pages/login-pages/signUp";
-// import Home from "./pages/canteen/Home.js";
+import AdminLogin from "./pages/login-pages/AdminLogin";
+import StudentLogin from "./pages/login-pages/StudentLogin";
+import Clgsearch from "./pages/college/Clgsearch";
+import Addcollege from "./pages/college/Addcollege";
+import SignUp from "./pages/login-pages/signUp";
+import Home from "./pages/canteen/Home.js";
 import Context from "./Context";
 import { useState } from "react";
 
@@ -22,18 +22,18 @@ function App() {
       <Router>
         <div className="App">
           <Switch>
-            {/* <Route exact path="/">
+            <Route exact path="/">
               <HeroPage />
-            </Route> */}
-            {/* <Route exact path="/adminlogin">
+            </Route>
+            <Route exact path="/adminlogin">
               <AdminLogin />
-            </Route> */}
-            {/* <Route exact path="/studentlogin">
+            </Route>
+            <Route exact path="/studentlogin">
               <StudentLogin />
-            </Route> */}
-            {/* <Route exact path="/signUp">
+            </Route>
+            <Route exact path="/signUp">
               <SignUp />
-            </Route> */}
+            </Route>
             <Route exact path="/stathome">
               <StatHome/>
             </Route>
@@ -41,7 +41,7 @@ function App() {
               <StatCart />
             </Route>
 
-            {/* <Route exact path="/Cant">
+            <Route exact path="/Cant">
               <CantHome />
             </Route>
             <Route exact path="/CantMenu">
@@ -58,7 +58,7 @@ function App() {
             </Route>
             <Route exact path="/home">
               <Home />
-            </Route> */}
+            </Route>
           </Switch>
         </div>
       </Router>
