82601001e33a1d34bd6623bbdb2f61b5a76ee884
Anubha13kumari <anubhamuz@gmail.com>
2022-09-11 14:01:18 +0530

    added payment method

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import "./App.css";
 import StatHome from "./pages/stationary/StatHome";
-import HeroPage from "./pages/login-pages/heroPage";
-import CantHome from "./pages/canteen/Home";
-import CantMenu from "./pages/canteen/Menu";
-import CantCart from "./pages/canteen/Cart";
+// import HeroPage from "./pages/login-pages/heroPage";
+// import CantHome from "./pages/canteen/Home";
+// import CantMenu from "./pages/canteen/Menu";
+// import CantCart from "./pages/canteen/Cart";
 import StatCart from "./pages/stationary/StatCart";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import AdminLogin from "./pages/login-pages/AdminLogin";
-import StudentLogin from "./pages/login-pages/StudentLogin";
-import Clgsearch from "./pages/college/Clgsearch";
-import Addcollege from "./pages/college/Addcollege";
-import SignUp from "./pages/login-pages/signUp";
-import Home from "./pages/canteen/Home.js";
+// import AdminLogin from "./pages/login-pages/AdminLogin";
+// import StudentLogin from "./pages/login-pages/StudentLogin";
+// import Clgsearch from "./pages/college/Clgsearch";
+// import Addcollege from "./pages/college/Addcollege";
+// import SignUp from "./pages/login-pages/signUp";
+// import Home from "./pages/canteen/Home.js";
 import Context from "./Context";
 import { useState } from "react";
 
@@ -22,7 +22,7 @@ function App() {
       <Router>
         <div className="App">
           <Switch>
-            <Route exact path="/">
+            {/* <Route exact path="/">
               <HeroPage />
             </Route>
             <Route exact path="/adminlogin">
@@ -33,7 +33,7 @@ function App() {
             </Route>
             <Route exact path="/signUp">
               <SignUp />
-            </Route>
+            </Route> */}
             <Route exact path="/stathome">
               <StatHome />
             </Route>
@@ -41,7 +41,7 @@ function App() {
               <StatCart />
             </Route>
 
-            <Route exact path="/Cant">
+            {/* <Route exact path="/Cant">
               <CantHome />
             </Route>
             <Route exact path="/CantMenu">
@@ -61,11 +61,11 @@ function App() {
             </Route>
             <Route exact path="/Cant">
               <CantHome />
-            </Route>
+            </Route> */}
           </Switch>
         </div>
       </Router>
-      //{" "}
+      
     </Context.Provider>
   );
 }
